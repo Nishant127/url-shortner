b73b61ef0c3c0ddc5700be5bbe66be82ab734724
Nishant127 <handgenishant@gmail.com>
2023-05-16 23:50:55 +0530

    NH|Create model schema to store urls.

--- url_shortner/apps/shortner/models.py
+++ url_shortner/apps/shortner/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from url_shortner.common.models import TimeStampedModel
 
-# Create your models here.
+
+class Shortner(TimeStampedModel):
+    original_url = models.URLField()
+    short_url = models.CharField(max_length=10, unique=True)
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return f"{self.original_url}-{self.short_url}"
